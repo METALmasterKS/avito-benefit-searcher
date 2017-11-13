38d0c0e0b1455eb14c7670bd56071319699c0031
Administrator <009@3259404.ru>
2017-11-13 23:20:07 +0300

    - монета, послед запросы

--- avitobenefitsearcher/avitobenefitsearcher.py
+++ avitobenefitsearcher/avitobenefitsearcher.py
@@ -1,6 +1,12 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
+import urllib.request
+
+
+response = urllib.request.urlopen('https://www.avito.ru/sankt-peterburg?pmax=2000&pmin=300&s=101&q=ssd')
+
+print(response.read())
 
 if __name__ == "__main__":
     print("Hello World")
